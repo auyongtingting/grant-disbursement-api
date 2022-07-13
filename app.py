eda3ff64f09de710c9df13eed81b6a5621de2b96
Au Yong Ting Ting <tingting.a@affinidi.com>
2022-07-14 02:32:10 +0800

    Amend SQLALCHEMY_DATABASE_URI

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if ENV =='dev':
     app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:root@localhost:5432/grant_disbursement'
 else: 
     app.debug=False
-    app.config['SQLALCHEMY_DATABASE_URI']='postgres://aotccantdkhwom:e0ee040a0d97f9cc97acc9caac0f2e11cbbf9857000898fbf665908561d26cdd@ec2-3-219-229-143.compute-1.amazonaws.com:5432/d74v0oq1fb30tv'
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://aotccantdkhwom:e0ee040a0d97f9cc97acc9caac0f2e11cbbf9857000898fbf665908561d26cdd@ec2-3-219-229-143.compute-1.amazonaws.com:5432/d74v0oq1fb30tv'
     
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
